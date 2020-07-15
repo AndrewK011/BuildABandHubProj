f733e866cd8545417beff708bd772e8a0b0a5b21
Andrew K <drummer4life93@yahoo.com>
2020-07-15 11:59:07 -0500

    init

---
+++ BuildABandHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuildABandHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
